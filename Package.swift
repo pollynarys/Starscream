2732b7edbea9a2f46a6c78796d26c4dab041de44
Polina Vdovina <polinavdovina@Polinas-MacBook-Pro.local>
2025-02-24 20:27:52 +0700

    Updated deps

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         ],
         dependencies: [
             .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "4.0.0"),
-            .package(url: "https://github.com/swiftlang/swift-corelibs-foundation", from: "1.0.0"),
+            .package(url: "https://github.com/swiftlang/swift-corelibs-foundation", from: "6.0.3"),
         ],
         targets: [
             .target(name: "Starscream",
